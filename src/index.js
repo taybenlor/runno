2c614c3107ac2484286a36024a93099a5aff99b2
Ben Taylor <me@taybenlor.com>
2021-06-06 16:43:34 +1000

    Take command and code from URL and run it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 // import "normalize.css";
-import { Component } from "preact";
-// import WasmTerminal from "@wasmer/wasm-terminal";
-import WasmTerminal from "@wasmer/wasm-terminal/lib/optimized/wasm-terminal.esm";
+import { Component, createRef } from "preact";
+import WasmTerminal from "@wasmer/wasm-terminal";
+// TODO: Use this version when deploying?
+// import WasmTerminal from "@wasmer/wasm-terminal/lib/optimized/wasm-terminal.esm";
 import { WasmFs } from "@wasmer/wasmfs";
 
 import WAPM from "./services/wapm/wapm";
 
 export default class App extends Component {
+  terminalRef = createRef();
+
   constructor() {
     super();
 
@@ -28,28 +31,49 @@ export default class App extends Component {
   }
 
   async fetchCommand(options) {
-    let commandName = options.args[0];
-    if (window.gtag) {
-      window.gtag("event", "run command", {
-        // 'event_category': '',
-        event_label: commandName,
-        // 'value': '<here the command args and environment>'
-      });
-    }
     return await this.wapm.runCommand(options);
   }
 
-  componentDidMount() {
-    const asyncTask = async () => {
-      let params = this._handleQueryParams();
-      if (params.runCommand) {
-        setTimeout(() => this.wasmTerminal.runCommand(params.runCommand), 50);
-      }
+  startCommandFromParams() {
+    let command = "echo unsupported command specified";
 
-      // TODO: There's a bug that prevents this promise from ever resolving
-      await this._setupWasmTerminal();
-    };
-    asyncTask();
+    const params = new URLSearchParams(window.location.search);
+    let code = params.get("code") || undefined;
+    if (params.get("wapm")) {
+      command = params.get("wapm");
+    } else if (params.get("runtime")) {
+      const runtime = params.get("runtime");
+      if (runtime === "python") {
+        command = "python";
+      }
+    } else {
+      // No command was specified
+      return;
+    }
+
+    this.startCommand(command, code);
+  }
+
+  /**
+   * Starts a runtime for execution
+   *
+   * @param {string} command - The name of the WAPM package to run
+   * @param {string=} code - The code to pass as a file
+   */
+  startCommand(command, code) {
+    this.wasmFs.volume.writeFileSync("/program", code);
+
+    // If theres code, pass it as the first argument to command
+    this.wasmTerminal.runCommand(`${command}${code ? " program" : ""}`);
+  }
+
+  //
+  // Preact Lifecycle Methods
+  //
+
+  componentDidMount() {
+    this.wasmTerminal.open(this.terminalRef.current);
+    setTimeout(() => this.startCommandFromParams(), 50);
   }
 
   componentWillUnmount() {
@@ -59,6 +83,7 @@ export default class App extends Component {
   }
 
   onResize() {
+    // TODO: Use a debouncer instead
     if (!this.resizing) {
       this.resizing = true;
       setTimeout(() => {
@@ -71,28 +96,8 @@ export default class App extends Component {
   render() {
     return (
       <div class="fullscreen">
-        <main id="wasm-terminal"></main>
+        <main ref={this.terminalRef} id="wasm-terminal"></main>
       </div>
     );
-  }
-
-  _setupWasmTerminal() {
-    // Let's bind our wasm terminal to it's container
-    const containerElement = document.querySelector("#wasm-terminal");
-    this.wasmTerminal.open(containerElement);
-
-    let resolveOpenPromise = undefined;
-    const openedPromise = new Promise((resolve) => {
-      resolveOpenPromise = resolve;
-    });
-
-    return openedPromise;
-  }
-
-  _handleQueryParams() {
-    const params = new URLSearchParams(window.location.search);
-    return {
-      runCommand: params.get("command"),
-    };
   }
 }
