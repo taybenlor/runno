093b76544659c71affd6d89317aeeed32040ad35
Ben Taylor <me@taybenlor.com>
2021-07-29 09:24:38 +1000

    Start of building out the design for the homepage

---
+++ packages/website/src/editor.ts
@@ -0,0 +1,51 @@
+import ConnectRunno, { Runtime, RunnoHost } from "@runno/host";
+
+const runButton = document.getElementById("run") as HTMLButtonElement;
+const headlessButton = document.getElementById(
+  "run-headless"
+) as HTMLButtonElement;
+const runtimeSelect = document.getElementById(
+  "runtime-select"
+) as HTMLSelectElement;
+const codeEl = document.getElementById("code") as HTMLTextAreaElement;
+const headlessStdinEl = document.getElementById(
+  "headless-stdin"
+) as HTMLTextAreaElement;
+const stdoutEl = document.getElementById("stdout") as HTMLPreElement;
+const stderrEl = document.getElementById("stderr") as HTMLPreElement;
+const stdinEl = document.getElementById("stdin") as HTMLPreElement;
+const ttyEl = document.getElementById("tty") as HTMLPreElement;
+const runtimeIframe = document.getElementById("runtime") as HTMLIFrameElement;
+runtimeIframe.src = import.meta.env.VITE_RUNTIME;
+
+ConnectRunno(runtimeIframe).then((runno: RunnoHost) => {
+  runButton.addEventListener("click", async function () {
+    const code = codeEl.value;
+
+    const { stdin, stdout, stderr, tty } = await runno.interactiveRunCode(
+      runtimeSelect.value as Runtime,
+      code
+    );
+
+    stdoutEl.textContent = stdout;
+    stdinEl.textContent = stdin;
+    stderrEl.textContent = stderr;
+    ttyEl.textContent = tty;
+  });
+
+  headlessButton.addEventListener("click", async function () {
+    const code = codeEl.value;
+    const codeStdin = headlessStdinEl.value;
+
+    const { stdin, stdout, stderr, tty } = await runno.headlessRunCode(
+      runtimeSelect.value as Runtime,
+      code,
+      codeStdin
+    );
+
+    stdoutEl.textContent = stdout;
+    stdinEl.textContent = stdin;
+    stderrEl.textContent = stderr;
+    ttyEl.textContent = tty;
+  });
+});
