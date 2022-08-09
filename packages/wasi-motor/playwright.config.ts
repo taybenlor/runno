d7841de9bbaecd2e06a60b74c10b76534101b2ce
Ben Taylor <me@taybenlor.com>
2022-08-09 22:23:41 +1000

    Get playwright running tests

--- packages/wasi-motor/playwright.config.ts
+++ packages/wasi-motor/playwright.config.ts
@@ -64,44 +64,17 @@ const config: PlaywrightTestConfig = {
         ...devices["Desktop Safari"],
       },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: "npm run test:server",
+    url: "http://localhost:5173/",
+    reuseExistingServer: !process.env.CI,
+  },
 };
 
 export default config;
--- packages/wasi-motor/tests/args.spec.ts
+++ packages/wasi-motor/tests/args.spec.ts
@@ -16,3 +16,33 @@
 //     err: errno
 
 // TODO: add tests using playwright to automate loading of the wasm binary and running it
+
+import { test, expect } from "@playwright/test";
+
+test("single-arg-is-hello gives non-zero exit code when run with no args", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173");
+
+  await page
+    .locator("select")
+    .selectOption("/programs/single-arg-is-hello.wasi.wasm");
+  await page.locator("text=Run").click();
+
+  await expect(page.locator("#exit-code")).not.toBeEmpty();
+  await expect(page.locator("#exit-code")).not.toHaveText("0");
+});
+
+test("single-arg-is-hello gives 0 exit code when run with hello as arg", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173");
+
+  await page
+    .locator("select")
+    .selectOption("/programs/single-arg-is-hello.wasi.wasm");
+  await page.locator("input#args").fill("hello");
+  await page.locator("text=Run").click();
+
+  await expect(page.locator("#exit-code")).toHaveText("0");
+});
