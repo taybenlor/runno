9a16a6421d1831867c8c024b487733aca85d9c88
Ben Taylor <ben.taylor@stileeducation.com>
2023-10-29 12:11:02 +1100

    Speed up tests, add regression test

--- packages/runtime/tests/headless.spec.ts
+++ packages/runtime/tests/headless.spec.ts
@@ -1,19 +1,15 @@
 import { test, expect } from "@playwright/test";
 import { RunResult } from "../lib/types";
+import { TestRunResult } from "../src/main";
 
 // TODO: These are dependent on `@runno/website` being run on localhost:4321
 // See: https://github.com/taybenlor/runno/issues/258
 
-// TODO: This python test times out, I think because of the massive base tar file.
-// When I comment out the base FS it works fine.
-test.skip("a simple python example", async ({ page }) => {
+test("a simple python example", async ({ page }) => {
   await page.goto("/");
 
   const result: RunResult = await page.evaluate(async () => {
-    return await globalThis.Runno.headlessRunCode(
-      "python",
-      `print("Hello, World!")`
-    );
+    return globalThis.test.headlessRunCode("python", `print("Hello, World!")`);
   });
 
   expect(result.resultType).toBe("complete");
@@ -26,9 +22,26 @@ test("a simple ruby example", async ({ page }) => {
   await page.goto("/");
 
   const result: RunResult = await page.evaluate(async () => {
-    return await globalThis.Runno.headlessRunCode(
-      "ruby",
-      `puts "Hello, World!"`
+    return globalThis.test.headlessRunCode("ruby", `puts "Hello, World!"`);
+  });
+
+  expect(result.resultType).toBe("complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  expect(result.stderr).toBe("");
+  expect(result.stdout).toBe("Hello, World!\n");
+});
+
+test("a simple C example", async ({ page }) => {
+  await page.goto("/");
+
+  const result: TestRunResult = await page.evaluate(async () => {
+    return globalThis.test.headlessRunCode(
+      "clang",
+      `#include <stdio.h>
+#include <string.h>
+int main() {
+  printf("Hello, World!\\n");
+}`
     );
   });
 
@@ -36,4 +49,26 @@ test("a simple ruby example", async ({ page }) => {
   if (result.resultType !== "complete") throw new Error("wtf");
   expect(result.stderr).toBe("");
   expect(result.stdout).toBe("Hello, World!\n");
+});
+
+test("declaring a C variable globally without initializer", async ({
+  page,
+}) => {
+  // https://github.com/taybenlor/runno/issues/282
+  await page.goto("/");
+
+  const result: RunResult = await page.evaluate(async () => {
+    return globalThis.test.headlessRunCode(
+      "clang",
+      `
+int error;
+int main() {
+}`
+    );
+  });
+
+  expect(result.resultType).toBe("complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  expect(result.stderr).toBe("");
+  expect(result.stdout).toBe("");
 });
