dfca4dd0b56a114dcb7990413bb9ab65db7e49f8
Ben Taylor <me@taybenlor.com>
2024-11-17 13:00:45 +1100

    Add simple integration tests for sandbox

---
+++ packages/sandbox/tests/sandbox.test.ts
@@ -0,0 +1,79 @@
+import { assertEquals, assertMatch } from "jsr:@std/assert";
+
+import { runCode } from "../lib/main.ts";
+
+Deno.test("a simple python example", async () => {
+  const result = await runCode("python", `print("Hello, World!")`);
+  assertEquals(result.resultType, "complete", JSON.stringify(result));
+  if (result.resultType !== "complete") throw new Error("wtf");
+  assertEquals(result.stderr, "");
+  assertEquals(result.stdout, "Hello, World!\n");
+});
+
+Deno.test("a simple ruby example", async () => {
+  const result = await runCode("ruby", `puts "Hello, World!"`);
+
+  assertEquals(result.resultType, "complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  assertEquals(result.stderr, "");
+  assertEquals(result.stdout, "Hello, World!\n");
+});
+
+Deno.test("a simple js example", async () => {
+  const result = await runCode("quickjs", `console.log("Hello, World!");`);
+
+  assertEquals(result.resultType, "complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  assertEquals(result.stderr, "");
+  assertEquals(result.stdout, "Hello, World!\n");
+});
+
+Deno.test("a simple php example", async () => {
+  const result = await runCode(
+    "php-cgi",
+    `
+<?php
+print "Hello, World!\n";
+?>
+`
+  );
+
+  assertEquals(result.resultType, "complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  assertEquals(result.stderr, "");
+  assertMatch(result.stdout, /Hello, World!\n/);
+});
+
+Deno.test("a simple C example", async () => {
+  const result = await runCode(
+    "clang",
+    `#include <stdio.h>
+#include <string.h>
+int main() {
+  printf("Hello, World!\\n");
+}`
+  );
+
+  assertEquals(result.resultType, "complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  assertEquals(result.stderr, "");
+  assertEquals(result.stdout, "Hello, World!\n");
+});
+
+Deno.test("a simple C++ example", async () => {
+  const result = await runCode(
+    "clangpp",
+    `
+#include <iostream>
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
+`
+  );
+
+  assertEquals(result.resultType, "complete");
+  if (result.resultType !== "complete") throw new Error("wtf");
+  assertEquals(result.stderr, "");
+  assertEquals(result.stdout, "Hello, World!\n");
+});
